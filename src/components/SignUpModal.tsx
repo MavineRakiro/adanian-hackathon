01c9d4df99785061db9c8e72c68d83f1efdac945
zidanegimiga <zidanegimiga@gmail.com>
2022-03-12 14:16:03 +0300

    setting up modals

---
+++ src/components/SignUpModal.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Overlay = styled.div`
+  position: relative;
+  width: 1728px;
+  height: 1117px;
+  background: rgba(201, 195, 195, 0.69);
+  backdrop-filter: blur(20px);
+`;
+
+const Modal = styled.div`
+  
+`;
+
+const SignUpModal = () => {
+  return (
+    <div>
+      Sign up Modal 
+    </div>
+  );
+};
+
+export default SignUpModal;
